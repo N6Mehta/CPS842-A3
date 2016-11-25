b76fd23024855c5729e5feadfc14bcdeb5cedfb5
Nitish Mehta <nitish.mehta33@gmail.com>
2016-11-25 09:24:09 -0500

    commit changes

---
+++ TestApplet/src/org/me/hello/MyApplet.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.hello;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ *
+ * @author Ni2
+ */
+public class MyApplet extends Applet {
+
+       @Override
+         public void paint(Graphics g) {
+             g.drawString("Hello applet!", 50, 25);
+         }
+}
+              
+
